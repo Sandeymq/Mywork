2508f083a3094c96859fb3872829954f49c5e8dd
Sandeymq <sashkogural@gmail.com>
2025-03-15 11:10:02 +0200

    First commit, added file main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+#class Cow:
+    #def __init__(self, name = None, age = 15):
+        #self.name = name
+        #self.age = age
+    #def GetInfo(self):
+        #print(f"Name is {self.name}. Age is {self.age}.")
+    #def Talk(self):
+        #print("Moo moo!")
+
+#obj = Cow("Cow", 15)
+#obj.Talk()
+#obj.GetInfo()
+#objg = Cow()
+#objg.GetInfo()
+
+#class Triangle:
+    #def __init__(self, width = 11, height = 10, dovzuna = 7):
+        #self.width = width
+        #self.height = height
+        #self.dovzuna = dovzuna
+    #def GetInfo(self):
+        #print(f"width {self.width}. height {self.height}. dovzuna {self.dovzuna}")
+    #def ploshca(self):
+        #print(self.width*self.height *0.5)
+    #def perimeter(self):
+        #print("24cm")
+
+#info = Triangle(11, 10, 7)
+#info1 = Triangle(115, 10, 7)
+#info.GetInfo()
+#info.ploshca()
+#infofo1.ploshca()
+
+
+
