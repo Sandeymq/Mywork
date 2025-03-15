c0aaa5b06016ebc719e7537d0b1c755d23fb7011
Sandeymq <sashkogural@gmail.com>
2025-03-15 11:35:05 +0200

    Second update at main.py

--- main.py
+++ main.py
@@ -30,6 +30,22 @@
 #info.GetInfo()
 #info.ploshca()
 #infofo1.ploshca()
-print("test commit")
+#print("test commit")
 
+class Student():
+    def __init__(self, name, age, grades = []):
+        self.name = name
+        self.age = age
+        self.grades = []
+    def add_grade(self, grade):
+        self.grades.append(grade)
+    def get_avg(self):
+        for i in self.grades:
+            sum += self.grades[i]
+
+
+student = Student("Олександр", 14)
+student.add_grade(90)
+student.add_grade(50)
+student.add_grade(20)
 
